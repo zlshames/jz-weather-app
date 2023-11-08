2fec7c82832957d2e0b1077a69e635581eea9abb
JT von Seggern <jtvon29@gmail.com>
2023-11-08 17:14:22 -0500

    More unneccessary changes

--- src/screens/home/mock/MockData.ts
+++ src/screens/home/mock/MockData.ts
@@ -1,32 +1,8 @@
 export default [
   {
-    name: "Daily News Stream",
-    description:
-      "Dynamic fully customizable search bar for both iOS and Android",
-    language: "Typescript",
-    star: 342,
-    fork: 152,
+    name: "Daily Weather Forecast",
   },
   {
-    name: "Weekly News Stream",
-    description: "Fully customizable animated bouncy checkbox",
-    language: "Typescript",
-    star: 272,
-    fork: 22,
-  },
-  {
-    name: "React Native Apple Card Views",
-    description:
-      "Customizable and perfect clone of Apple, App Store Card Views",
-    language: "Typescript",
-    star: 266,
-    fork: 22,
-  },
-  {
-    name: "React Native Header View",
-    description: "Fully customizable Header View with multiple design options ",
-    language: "Typescript",
-    star: 412,
-    fork: 28,
+    name: "Weekly Weather Forecast",
   },
 ];
